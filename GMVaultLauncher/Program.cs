678efd531558eac36e730e6b984b37cf5330e668
Unknown <alex.pantec@gmail.com>
2017-10-30 22:08:53 +0200

    take app and log dir path from args

--- GMVaultLauncher/Program.cs
+++ GMVaultLauncher/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace GMVaultLauncher
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace GMVaultLauncher
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            new Launcher(@"C:\Users\Alex\AppData\Local\gmvault\gmvault.bat", args, @"C:\Users\Alex\GDrive\Backup\gmvault\logs\").Launch();
+            if (args.Length < 2)
+            {
+                string appName = Process.GetCurrentProcess().ProcessName;
+                Console.WriteLine($"Usage: {appName} app_path log_dir args");
+                Console.WriteLine($@"Example: {appName} gmvault.bat logs\ sync -t quick name@gmail.com -e");
+                return 1;
+            }
+
+            new Launcher(args[0], args.Skip(2).ToArray(), args[1]).Launch();
+
+            return 0;
         }
     }
 }
